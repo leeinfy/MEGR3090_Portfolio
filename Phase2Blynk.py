d93c4708fe15f50190b8d44f580dc6cfe420e5b2
leeinfy <66580144+leeinfy@users.noreply.github.com>
2021-07-22 00:29:57 -0400

    Add files via upload

--- Phase2Blynk.py
+++ Phase2Blynk.py
@@ -8,33 +8,47 @@ BLYNK_AUTH = 'B8ltEMSYdnD9pn0rWOC_vC4oXMNBDecF'
 # initialize blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
-joystick_x_position = 128
-joystick_y_position = 128
-state = 0
-left_motor_speed = 0
-right_motor_speed = 0
+#disable warning
+GPIO.setwarnings(False)
+
+#raspberry GPIO configeration
 GPIO.setmode(GPIO.BCM)
 
+#left motor initialize
+left_motor_pin1 = 17
+left_motor_pin2 = 27
+left_motor_speed_pin = 22
+GPIO.setup(left_motor_pin1, GPIO.OUT)
+GPIO.setup(left_motor_pin2, GPIO.OUT)
+GPIO.setup(left_motor_speed_pin, GPIO.OUT)
+leftMotorPWM = GPIO.PWM(left_motor_speed_pin, 100)
+leftMotorPWM.start(0)
+
+#horizontal movement servo of camera mount initialize
 horizontal_servo = 2
 GPIO.setup(horizontal_servo, GPIO.OUT)
 horizontalServoPWM =  GPIO.PWM(horizontal_servo,50)
 horizontalServoPWM.start(0)
 
-# register handler for virtual pin V0 write event
+#global variable
+joystick_X_position = 128
+joystick_Y_position = 128
+
+# register V0 write for joystick X value
 @blynk.handle_event('write V0')
 def write_virtual_pin_handler(pin, value):
     value = int(format(value[0]))
-    global joystick_x_position
-    joystick_x_position = value
-
+    global joystick_X_position
+    joystick_X_position = value
+    
 # register handler for virtual pin V0 write event
 @blynk.handle_event('write V1')
 def write_virtual_pin_handler(pin, value):
     value = int(format(value[0]))
-    global joystick_y_position
-    joystick_y_position = value
+    global joystick_Y_position
+    joystick_Y_position = value
 
-# register handler for horizontal slider of camera servo
+# register V2 write for horizontal slider of camera servo
 @blynk.handle_event('write V2')
 def write_virtual_pin_handler(pin, value):
     slidervalue = float(format(value[0]))
@@ -44,8 +58,35 @@ def write_virtual_pin_handler(pin, value):
 
 # slider value to duty cycle convert equation
 def SliderNumberToServoDutyCycle(value):
-    return 3 + value
+    #slider value is from 0 to 10 and the servo duty cycle is from 3 to 13
+    # duty cycle 7.5, and slider value 4.5 is middle point
+    return 3 + value   
 
-       
+# left Motor state base on the joystick position
+def leftMotorRun(xValue, yValue):
+    if 100< xValue <155: #joystick X is in the middle
+        if 0 <= yValue <= 100: #joystick y is upward motor go forward
+            GPIO.output(left_motor_pin1, 0)
+            GPIO.output(left_motor_pin2, 1)
+            leftMotorPWM.ChangeDutyCycle(JoystickNumberToMotorDutyCycle(yValue))
+        elif 100< yValue <155: #joystick y is in the middle motor stop
+            GPIO.output(left_motor_pin1, 0)
+            GPIO.output(left_motor_pin2, 0)
+            leftMotorPWM.ChangeDutyCycle(0)
+        elif 155 <= yValue <=255: #joystick y is downward motor go downward
+            GPIO.output(left_motor_pin1, 1)
+            GPIO.output(left_motor_pin2, 0)
+            leftMotorPWM.ChangeDutyCycle(JoystickNumberToMotorDutyCycle(yValue))
+        
+def JoystickNumberToMotorDutyCycle(value):
+    if value >= 155:
+        dutycycle = value - 155
+    elif value <= 100:
+        dutycycle = 100 -value
+    return dutycycle
+        
+                     
 while True:
     blynk.run()
+    leftMotorRun(joystick_X_position, joystick_Y_position)
+    
